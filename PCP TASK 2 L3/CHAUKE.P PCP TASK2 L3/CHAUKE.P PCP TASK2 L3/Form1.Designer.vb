c1420a4a0e04131b3b73c056a02ece18fee29e67
prayer037 <jimmyprayer037@gmail.com>
2026-05-15 20:03:37 +0200

    Add files via upload

---
+++ PCP TASK 2 L3/CHAUKE.P PCP TASK2 L3/CHAUKE.P PCP TASK2 L3/Form1.Designer.vb
@@ -0,0 +1,287 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnWrite = New System.Windows.Forms.Button()
+        Me.btnRead = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.btnSort = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.mskContact = New System.Windows.Forms.MaskedTextBox()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.txtSurname = New System.Windows.Forms.TextBox()
+        Me.txtNames = New System.Windows.Forms.TextBox()
+        Me.txtAddress = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.lstDisplay = New System.Windows.Forms.ListBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnWrite
+        '
+        Me.btnWrite.Location = New System.Drawing.Point(81, 254)
+        Me.btnWrite.Name = "btnWrite"
+        Me.btnWrite.Size = New System.Drawing.Size(99, 23)
+        Me.btnWrite.TabIndex = 0
+        Me.btnWrite.Text = "Write to textfile"
+        Me.btnWrite.UseVisualStyleBackColor = True
+        '
+        'btnRead
+        '
+        Me.btnRead.Location = New System.Drawing.Point(204, 254)
+        Me.btnRead.Name = "btnRead"
+        Me.btnRead.Size = New System.Drawing.Size(131, 23)
+        Me.btnRead.TabIndex = 1
+        Me.btnRead.Text = "Read from the textfile"
+        Me.btnRead.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(567, 367)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 2
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Location = New System.Drawing.Point(212, 30)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(75, 23)
+        Me.btnSearch.TabIndex = 3
+        Me.btnSearch.Text = "Search"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'btnSort
+        '
+        Me.btnSort.Location = New System.Drawing.Point(449, 367)
+        Me.btnSort.Name = "btnSort"
+        Me.btnSort.Size = New System.Drawing.Size(75, 23)
+        Me.btnSort.TabIndex = 4
+        Me.btnSort.Text = "Sort"
+        Me.btnSort.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(737, 477)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 33)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.mskContact)
+        Me.GroupBox1.Controls.Add(Me.txtEmail)
+        Me.GroupBox1.Controls.Add(Me.txtSurname)
+        Me.GroupBox1.Controls.Add(Me.txtNames)
+        Me.GroupBox1.Controls.Add(Me.txtAddress)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Location = New System.Drawing.Point(48, 76)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(333, 172)
+        Me.GroupBox1.TabIndex = 6
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Customer Details"
+        '
+        'mskContact
+        '
+        Me.mskContact.Location = New System.Drawing.Point(103, 106)
+        Me.mskContact.Mask = "(999) 000-0000"
+        Me.mskContact.Name = "mskContact"
+        Me.mskContact.Size = New System.Drawing.Size(196, 20)
+        Me.mskContact.TabIndex = 17
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(103, 75)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(215, 20)
+        Me.txtEmail.TabIndex = 16
+        '
+        'txtSurname
+        '
+        Me.txtSurname.Location = New System.Drawing.Point(103, 49)
+        Me.txtSurname.Name = "txtSurname"
+        Me.txtSurname.Size = New System.Drawing.Size(215, 20)
+        Me.txtSurname.TabIndex = 15
+        '
+        'txtNames
+        '
+        Me.txtNames.Location = New System.Drawing.Point(103, 23)
+        Me.txtNames.Name = "txtNames"
+        Me.txtNames.Size = New System.Drawing.Size(215, 20)
+        Me.txtNames.TabIndex = 14
+        '
+        'txtAddress
+        '
+        Me.txtAddress.Location = New System.Drawing.Point(103, 139)
+        Me.txtAddress.Name = "txtAddress"
+        Me.txtAddress.Size = New System.Drawing.Size(215, 20)
+        Me.txtAddress.TabIndex = 13
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(63, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Names:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(54, 52)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(52, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Surname:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(30, 78)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(76, 13)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Email Address:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(59, 109)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(47, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Contact:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(16, 142)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(90, 13)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Physical Address:"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.txtSearch)
+        Me.GroupBox2.Controls.Add(Me.btnSearch)
+        Me.GroupBox2.Location = New System.Drawing.Point(48, 291)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(333, 81)
+        Me.GroupBox2.TabIndex = 0
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Search Customer here"
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Location = New System.Drawing.Point(19, 32)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(167, 20)
+        Me.txtSearch.TabIndex = 8
+        '
+        'lstDisplay
+        '
+        Me.lstDisplay.FormattingEnabled = True
+        Me.lstDisplay.Location = New System.Drawing.Point(401, 102)
+        Me.lstDisplay.Name = "lstDisplay"
+        Me.lstDisplay.Size = New System.Drawing.Size(411, 251)
+        Me.lstDisplay.TabIndex = 7
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.Label6.Location = New System.Drawing.Point(104, 9)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(324, 55)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "Takealot.com"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(979, 547)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.lstDisplay)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnSort)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnRead)
+        Me.Controls.Add(Me.btnWrite)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnWrite As Button
+    Friend WithEvents btnRead As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents btnSort As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents lstDisplay As ListBox
+    Friend WithEvents mskContact As MaskedTextBox
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents txtSurname As TextBox
+    Friend WithEvents txtNames As TextBox
+    Friend WithEvents txtAddress As TextBox
+    Friend WithEvents txtSearch As TextBox
+    Friend WithEvents Label6 As Label
+End Class
---
+++ PCP TASK 2 L3/CHAUKE.P PCP TASK2 L3/CHAUKE.P PCP TASK2 L3/Form1.vb
@@ -0,0 +1,176 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+
+    ' SUB to save customer details
+    Private Sub SaveCustomerDetails()
+
+        Dim filePath As String = "C:\Principles of Computer Programming\PCP TASK 2 L3\CUSTOMER DETAILS.txt"
+
+        ' Get values from textboxes
+        Dim firstName As String
+        Dim surname As String
+        Dim email As String
+        Dim phoneNumber As String
+        Dim PhysicalAddress As String
+
+        firstName = txtNames.Text
+        surname = txtSurname.Text
+        email = txtEmail.Text
+        phoneNumber = mskContact.Text
+        PhysicalAddress = txtAddress.Text
+
+
+        ' Create line to save
+        Dim customerRecord As String =
+            firstName & "," &
+            surname & "," &
+            email & "," &
+            phoneNumber
+
+        MessageBox.Show("Customer details saved!")
+        If txtNames.Text = "" Then
+            MessageBox.Show("Enter Name!")
+        End If
+        If txtSurname.Text = "" Then
+            MessageBox.Show("Enter Surname!")
+        End If
+        If txtEmail.Text = "" Then
+            MessageBox.Show("Enter Email!")
+        End If
+        If txtAddress.Text = "" Then
+            MessageBox.Show("Enter Address!")
+        End If
+        If mskContact.Text = "" Then
+            MessageBox.Show("Enter Phone number!")
+        End If
+        ' Write to file
+        Using writer As StreamWriter = New StreamWriter(filePath, True)
+            writer.WriteLine(customerRecord)
+        End Using
+
+    End Sub
+
+
+    Private Sub btnWrite_Click(sender As Object, e As EventArgs) Handles btnWrite.Click
+        SaveCustomerDetails()
+    End Sub
+
+    Private Sub ReadCustomersDetails()
+
+        ' Clear existing items
+        lstDisplay.Items.Clear()
+
+        Dim filePath As String = "C:\Principles of Computer Programming\PCP TASK 2 L3\CUSTOMER DETAILS.txt"
+
+        ' Check if file exists
+        If File.Exists(filePath) Then
+
+            ' Read all lines from file
+            Dim lines() As String = File.ReadAllLines(filePath)
+
+            ' Add each line to the ListBox
+            For Each line As String In lines
+                lstDisplay.Items.Add(line)
+            Next
+
+        Else
+            MessageBox.Show("Customer file not found.")
+        End If
+
+
+    End Sub
+
+    Private Sub btnRead_Click(sender As Object, e As EventArgs) Handles btnRead.Click
+        ReadCustomersDetails()
+    End Sub
+
+    Private Sub SortCustomerFile()
+
+        Dim filePath As String = "C:\Principles of Computer Programming\PCP TASK 2 L3\CUSTOMER DETAILS.txt"
+
+        ' Check if file exists
+        If File.Exists(filePath) Then
+
+            ' Read all lines
+            Dim lines() As String = File.ReadAllLines(filePath)
+
+            ' Sort alphabetically
+            Dim sortedLines = lines.OrderBy(Function(x) x).ToArray()
+
+            ' Write sorted lines back to file
+            File.WriteAllLines(filePath, sortedLines)
+
+            MessageBox.Show("Customer file sorted!")
+
+        Else
+            MessageBox.Show("File not found.")
+        End If
+
+    End Sub
+
+    Private Sub btnSort_Click(sender As Object, e As EventArgs) Handles btnSort.Click
+        SortCustomerFile()
+    End Sub
+
+    Private Sub SearchCustomer()
+
+        Dim filePath As String = "C:\Principles of Computer Programming\PCP TASK 2 L3\CUSTOMER DETAILS.txt"
+        Dim searchText As String = txtSearch.Text.ToLower()
+
+        ' Clear previous results
+        lstDisplay.Items.Clear()
+
+        ' Check if file exists
+        If File.Exists(filePath) Then
+
+            ' Read all lines
+            Dim lines() As String = File.ReadAllLines(filePath)
+
+            ' Search through lines
+            For Each line As String In lines
+
+                If line.ToLower().Contains(searchText) Then
+                    lstDisplay.Items.Add(line)
+                End If
+
+            Next
+
+            ' If nothing found
+            If lstDisplay.Items.Count = 0 Then
+                MessageBox.Show("No customer found.")
+            End If
+
+        Else
+            MessageBox.Show("File not found.")
+        End If
+
+    End Sub
+
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        SearchCustomer()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        Dim intanswer As Integer
+        intanswer = MessageBox.Show("Are you sure?", "Clear", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If intanswer = DialogResult.Yes Then
+            txtNames.Clear()
+            txtSurname.Clear()
+            txtEmail.Clear()
+            txtAddress.Clear()
+            txtSearch.Clear()
+            lstDisplay.Items.Clear()
+        End If
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Dim intanswer As Integer
+        intanswer = MessageBox.Show("Are you sure?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If intanswer = DialogResult.Yes Then
+            'terminate the program
+            Application.Exit()
+        End If
+    End Sub
+End Class
