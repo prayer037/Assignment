3f7ca1617f527e704d86ec154157fc46b3e0acf5
prayer037 <jimmyprayer037@gmail.com>
2026-05-29 15:29:40 +0200

    Add files via upload

---
+++ Chauke Prayer TASK 1 CPROG/Chauke Prayer TASK 1 CPROG/Form1.Designer.vb
@@ -0,0 +1,341 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.rbMonitor = New System.Windows.Forms.RadioButton()
+        Me.rbLaptop = New System.Windows.Forms.RadioButton()
+        Me.rbRouter = New System.Windows.Forms.RadioButton()
+        Me.rbComputerCase = New System.Windows.Forms.RadioButton()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.chkMouse = New System.Windows.Forms.CheckBox()
+        Me.chkPrinter = New System.Windows.Forms.CheckBox()
+        Me.chkKeyboard = New System.Windows.Forms.CheckBox()
+        Me.chkHDMICable = New System.Windows.Forms.CheckBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.txtContact = New System.Windows.Forms.TextBox()
+        Me.txtSurname = New System.Windows.Forms.TextBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.Display = New System.Windows.Forms.ListBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(490, 417)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(143, 32)
+        Me.btnCalculate.TabIndex = 0
+        Me.btnCalculate.Text = "Calculate Promotion"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(634, 481)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(84, 32)
+        Me.btnExit.TabIndex = 1
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(383, 417)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(80, 32)
+        Me.btnClear.TabIndex = 2
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.rbMonitor)
+        Me.GroupBox1.Controls.Add(Me.rbLaptop)
+        Me.GroupBox1.Controls.Add(Me.rbRouter)
+        Me.GroupBox1.Controls.Add(Me.rbComputerCase)
+        Me.GroupBox1.Location = New System.Drawing.Point(27, 171)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(316, 143)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Group A Items"
+        '
+        'rbMonitor
+        '
+        Me.rbMonitor.AutoSize = True
+        Me.rbMonitor.Location = New System.Drawing.Point(46, 38)
+        Me.rbMonitor.Name = "rbMonitor"
+        Me.rbMonitor.Size = New System.Drawing.Size(60, 17)
+        Me.rbMonitor.TabIndex = 10
+        Me.rbMonitor.TabStop = True
+        Me.rbMonitor.Text = "Monitor"
+        Me.rbMonitor.UseVisualStyleBackColor = True
+        '
+        'rbLaptop
+        '
+        Me.rbLaptop.AutoSize = True
+        Me.rbLaptop.Location = New System.Drawing.Point(46, 61)
+        Me.rbLaptop.Name = "rbLaptop"
+        Me.rbLaptop.Size = New System.Drawing.Size(58, 17)
+        Me.rbLaptop.TabIndex = 11
+        Me.rbLaptop.TabStop = True
+        Me.rbLaptop.Text = "Laptop"
+        Me.rbLaptop.UseVisualStyleBackColor = True
+        '
+        'rbRouter
+        '
+        Me.rbRouter.AutoSize = True
+        Me.rbRouter.Location = New System.Drawing.Point(46, 84)
+        Me.rbRouter.Name = "rbRouter"
+        Me.rbRouter.Size = New System.Drawing.Size(57, 17)
+        Me.rbRouter.TabIndex = 12
+        Me.rbRouter.TabStop = True
+        Me.rbRouter.Text = "Router"
+        Me.rbRouter.UseVisualStyleBackColor = True
+        '
+        'rbComputerCase
+        '
+        Me.rbComputerCase.AutoSize = True
+        Me.rbComputerCase.Location = New System.Drawing.Point(46, 107)
+        Me.rbComputerCase.Name = "rbComputerCase"
+        Me.rbComputerCase.Size = New System.Drawing.Size(97, 17)
+        Me.rbComputerCase.TabIndex = 13
+        Me.rbComputerCase.TabStop = True
+        Me.rbComputerCase.Text = "Computer Case"
+        Me.rbComputerCase.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.chkMouse)
+        Me.GroupBox2.Controls.Add(Me.chkPrinter)
+        Me.GroupBox2.Controls.Add(Me.chkKeyboard)
+        Me.GroupBox2.Controls.Add(Me.chkHDMICable)
+        Me.GroupBox2.Location = New System.Drawing.Point(27, 320)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(316, 129)
+        Me.GroupBox2.TabIndex = 0
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Group B Items"
+        '
+        'chkMouse
+        '
+        Me.chkMouse.AutoSize = True
+        Me.chkMouse.Location = New System.Drawing.Point(46, 31)
+        Me.chkMouse.Name = "chkMouse"
+        Me.chkMouse.Size = New System.Drawing.Size(58, 17)
+        Me.chkMouse.TabIndex = 9
+        Me.chkMouse.Text = "Mouse"
+        Me.chkMouse.UseVisualStyleBackColor = True
+        '
+        'chkPrinter
+        '
+        Me.chkPrinter.AutoSize = True
+        Me.chkPrinter.Location = New System.Drawing.Point(190, 31)
+        Me.chkPrinter.Name = "chkPrinter"
+        Me.chkPrinter.Size = New System.Drawing.Size(56, 17)
+        Me.chkPrinter.TabIndex = 10
+        Me.chkPrinter.Text = "Printer"
+        Me.chkPrinter.UseVisualStyleBackColor = True
+        '
+        'chkKeyboard
+        '
+        Me.chkKeyboard.AutoSize = True
+        Me.chkKeyboard.Location = New System.Drawing.Point(46, 69)
+        Me.chkKeyboard.Name = "chkKeyboard"
+        Me.chkKeyboard.Size = New System.Drawing.Size(71, 17)
+        Me.chkKeyboard.TabIndex = 11
+        Me.chkKeyboard.Text = "Keyboard"
+        Me.chkKeyboard.UseVisualStyleBackColor = True
+        '
+        'chkHDMICable
+        '
+        Me.chkHDMICable.AutoSize = True
+        Me.chkHDMICable.Location = New System.Drawing.Point(190, 69)
+        Me.chkHDMICable.Name = "chkHDMICable"
+        Me.chkHDMICable.Size = New System.Drawing.Size(84, 17)
+        Me.chkHDMICable.TabIndex = 12
+        Me.chkHDMICable.Text = "HDMI Cable"
+        Me.chkHDMICable.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(43, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(29, 63)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(52, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Surname:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(309, 63)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(79, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Phone number:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(353, 34)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(35, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Email:"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.txtEmail)
+        Me.GroupBox3.Controls.Add(Me.txtContact)
+        Me.GroupBox3.Controls.Add(Me.txtSurname)
+        Me.GroupBox3.Controls.Add(Me.txtName)
+        Me.GroupBox3.Controls.Add(Me.Label3)
+        Me.GroupBox3.Controls.Add(Me.Label4)
+        Me.GroupBox3.Controls.Add(Me.Label2)
+        Me.GroupBox3.Controls.Add(Me.Label1)
+        Me.GroupBox3.Location = New System.Drawing.Point(27, 50)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(691, 100)
+        Me.GroupBox3.TabIndex = 8
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Customer Details"
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(394, 31)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(144, 20)
+        Me.txtEmail.TabIndex = 11
+        '
+        'txtContact
+        '
+        Me.txtContact.Location = New System.Drawing.Point(394, 60)
+        Me.txtContact.Name = "txtContact"
+        Me.txtContact.Size = New System.Drawing.Size(144, 20)
+        Me.txtContact.TabIndex = 10
+        '
+        'txtSurname
+        '
+        Me.txtSurname.Location = New System.Drawing.Point(87, 60)
+        Me.txtSurname.Name = "txtSurname"
+        Me.txtSurname.Size = New System.Drawing.Size(134, 20)
+        Me.txtSurname.TabIndex = 9
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(87, 31)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(134, 20)
+        Me.txtName.TabIndex = 8
+        '
+        'Display
+        '
+        Me.Display.FormattingEnabled = True
+        Me.Display.Location = New System.Drawing.Point(371, 171)
+        Me.Display.Name = "Display"
+        Me.Display.Size = New System.Drawing.Size(347, 225)
+        Me.Display.TabIndex = 9
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Tahoma", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Label5.Location = New System.Drawing.Point(20, 5)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(255, 42)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Takealot.com"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(821, 525)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Display)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents txtContact As TextBox
+    Friend WithEvents txtSurname As TextBox
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents chkMouse As CheckBox
+    Friend WithEvents chkPrinter As CheckBox
+    Friend WithEvents chkKeyboard As CheckBox
+    Friend WithEvents chkHDMICable As CheckBox
+    Friend WithEvents Display As ListBox
+    Friend WithEvents rbMonitor As RadioButton
+    Friend WithEvents rbLaptop As RadioButton
+    Friend WithEvents rbRouter As RadioButton
+    Friend WithEvents rbComputerCase As RadioButton
+    Friend WithEvents Label5 As Label
+End Class
---
+++ Chauke Prayer TASK 1 CPROG/Chauke Prayer TASK 1 CPROG/Form1.vb
@@ -0,0 +1,133 @@
+﻿Public Class Form1
+    Dim totalPrice As Double = 0
+    Const VAT As Double = 0.15
+    Dim CountGroupB As Integer = 0
+
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        Dim intAnswer As Integer
+        intAnswer = MessageBox.Show("Are you sure you want to clear!", "Clear", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If intAnswer = DialogResult.Yes Then
+            'Clears all the inputs and outputs
+            txtName.Clear()
+            txtSurname.Clear()
+            txtEmail.Clear()
+            txtContact.Clear()
+            rbComputerCase.Checked = False
+            rbLaptop.Checked = False
+            rbMonitor.Checked = False
+            rbRouter.Checked = False
+            chkHDMICable.Checked = False
+            chkKeyboard.Checked = False
+            chkMouse.Checked = False
+            chkPrinter.Checked = False
+            Display.Items.Clear()
+            txtName.Focus()
+        End If
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Dim intAnswer As Integer
+        intAnswer = MessageBox.Show("Are you sure you want to exit!", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If intAnswer = DialogResult.Yes Then
+            'Closes the program
+            Application.Exit()
+        End If
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+        Dim DiscountAmount As Double
+
+        If chkHDMICable.Checked Then CountGroupB += 1
+        If chkKeyboard.Checked Then CountGroupB += 1
+        If chkMouse.Checked Then CountGroupB += 1
+        If chkPrinter.Checked Then CountGroupB += 1
+
+
+        DiscountAmount = calcDiscount()
+
+        Display.Items.Add("................TAKEALOT STORE.................")
+        Display.Items.Add("NAME    :" & txtName.Text())
+        Display.Items.Add("SURNAME :" & txtSurname.Text())
+        Display.Items.Add("EMAIL   :" & txtEmail.Text())
+        Display.Items.Add("CONTACT :" & txtContact.Text())
+        Display.Items.Add("")
+        Display.Items.Add("==============================================")
+        Display.Items.Add("")
+        If rbMonitor.Checked Then
+            Display.Items.Add("MONITOR........................:R2 000")
+        End If
+        If rbLaptop.Checked Then
+            Display.Items.Add("LAPTOP.........................:R5 500")
+        End If
+        If rbComputerCase.Checked Then
+            Display.Items.Add("COMPUTER CASE..................:R3 000")
+        End If
+        If rbRouter.Checked Then
+            Display.Items.Add("ROUTER.........................:R900")
+        End If
+        If chkHDMICable.Checked Then
+            Display.Items.Add("HDMI CABLE.....................:R400")
+        End If
+        If chkKeyboard.Checked Then
+            Display.Items.Add("KEYBOARD.......................:R250")
+        End If
+        If chkMouse.Checked Then
+            Display.Items.Add("MOUSE..........................:R100")
+        End If
+        If chkPrinter.Checked Then
+            Display.Items.Add("PRINTER........................:R1 300")
+        End If
+
+        Display.Items.Add("Sub total.......................:R" & totalAmount())
+        Display.Items.Add("DISCOUNT AMAOUNT.................:R" & DiscountAmount)
+        Display.Items.Add("VAT RATE..........................: R" & calcVAT())
+        Display.Items.Add("TOTAL PRICE TO PAY :R" & Amount())
+
+    End Sub
+    Public Function totalAmount() As Integer
+
+        totalPrice = 0
+        'Group A items and prices
+        If rbComputerCase.Checked Then totalPrice += 3000
+        If rbLaptop.Checked Then totalPrice += 5500
+        If rbMonitor.Checked Then totalPrice += 2000
+        If rbRouter.Checked Then totalPrice += 900
+
+        'Group B items and prices
+        If chkHDMICable.Checked Then totalPrice += 400
+        If chkKeyboard.Checked Then totalPrice += 250
+        If chkMouse.Checked Then totalPrice += 100
+        If chkPrinter.Checked Then totalPrice += 1300
+
+        Return totalPrice
+    End Function
+    Private Function calcDiscount() As Integer
+        ' 0 Group B = No discount
+        If CountGroupB = 0 Then
+            Return 0
+
+            ' 1 Group B = 5% discount
+        ElseIf CountGroupB = 1 Then
+            Return totalPrice * 0.05
+
+            ' 2 or more Group B = 15% discount
+        Else
+            Return totalPrice * 0.15
+        End If
+
+    End Function
+    Private Function calcVAT() As Double
+        ' Calculating the 15% VAT rate
+        Dim VATrate As Double
+        VATrate = totalPrice * 0.15
+        Return VATrate
+    End Function
+    Private Function Amount() As Integer
+        'Calculating the Final amount to be payed
+        Dim Amount_to_Pay As Integer
+        Amount_to_Pay = totalPrice - CountGroupB + 0.15
+        Return Amount_to_Pay
+    End Function
+End Class
